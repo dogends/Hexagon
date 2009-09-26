6d3015283ba71fedfcfb1940245bb98c4647aaef
dogends <contact@dandiggins.co.uk>
2009-09-26 13:22:10 -0700

    changes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #ifdef __APPLE__
-#include <GLUT/glut.h>
+//#include <GLUT/glut.h>
 #else
-#include <GL/glut.h>
+//#include <GL/glut.h>
 #endif
 #include <math.h>
 #include "hexagons.h"
+
+
+#include <GL/glut.h>
+
 
 GLfloat rot = 0.0f;
 GLfloat lightPos[] = { 0.0f, 0.0f, 0.0f, 1.0f };
@@ -22,26 +26,26 @@ Hexagons hexagons;
 
 /* opengl drawing event handler */
 void onRender(void) {
-	
+
 	/* set clear colour to black */
     glClearColor(0.0, 0.0, 0.0, 1.0);
-	
+
 	/* clear the buffer (to black) */
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
+
 	glEnable( GL_DEPTH_TEST );
 	glFrontFace( GL_CW );
 	//glEnable( GL_CULL_FACE );
-	
+
 	glEnable(GL_LIGHTING);
-	
+
 	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight );
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight );
-	
+
 	glEnable( GL_LIGHT0 );
 	glEnable( GL_COLOR_MATERIAL );
 	glColorMaterial( GL_FRONT, GL_AMBIENT_AND_DIFFUSE );
-	
+
 	// Draw hexagon 1
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -60,7 +64,7 @@ void onRender(void) {
 	glRotatef( rot, 1.0f, 0.0, 0.0 );
 	glColor3f( 0.0f, 1.0f, 0.0f );
 	glCallList(1);
-	
+
 	// Draw hexagon 3
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -70,28 +74,30 @@ void onRender(void) {
 	glRotatef( rot, 1.0f, 0.0, 0.0 );
 	glColor3f( 0.0f, 0.0f, 1.0f );
 	glCallList(1);
-	
+
 	/* flush everything in the current pipeline*/
-    glFlush();
+ //   glFlush();
+
+    glutSwapBuffers();
 }
 
 /* opengl window resize event handler */
 void onResize(int w, int h) {
-	
+
 	// Prevent a divide by zero */
 	if(h == 0) h = 1;
-	
+
 	// Set the viewport to be the entire window
 	glViewport(0, 0, w, h);
-	
+
 	GLfloat aspectRatio = (GLfloat)w/ (GLfloat)h;
-	
+
 	// Reset the coordinate system before modifying
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	
+
 	gluPerspective(60.0f, aspectRatio, 10.0f, 200.0f );
-	
+
 }
 
 /* timer event handler */
@@ -107,44 +113,44 @@ void setup() {
 
 		glBegin( GL_TRIANGLE_FAN );
 		glVertex3f( 0.0f, 0.0f, 0.0f );
-		for (int p=0; p<7; p++ ) 
+		for (int p=0; p<7; p++ )
 		{
 			GLfloat angle = 30.0f + (p*60.0f);
-		
+
 			GLfloat x = 1.0f*sin(deg_to_rad(angle));
 			GLfloat y = 1.0f*cos(deg_to_rad(angle));
-		
+
 			glVertex3f( x, y, 0.0f );
 		}
 		glEnd();
-	
+
 	glEndList();
 }
 
 int main (int argc, char * argv[]) {
-	
+
 	/* Initialise glut with any passed command line options */
 	glutInit(&argc, argv);
-	
-	glutInitDisplayMode( GLUT_DEPTH );
-	
+
+	glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH );
+
 	/* Create a window to display the opengl output */
 	glutCreateWindow( "GLUT 02" );
-	
+
 	/* Define the function that will be called when the opengl stuff needs to be drawn */
 	glutDisplayFunc( onRender );
-	
+
 	/* Define the function that will be called when the window is resized */
     glutReshapeFunc( onResize );
-	
+
 	/* register a timer function to increment the rotations */
 	glutTimerFunc( 10, onTimer, 1 );
-	
+
 	/* init some stuff */
 	setup();
-	
+
 	/* Start GLUT processing */
 	glutMainLoop();
-	
+
     return 0;
 }
